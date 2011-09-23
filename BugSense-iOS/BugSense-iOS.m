aef02fff5fe8a1b9826eca6a72d0331a9bfc0889
Nick Toumpelis <nick@toumpelis.me.uk>
2011-09-23 16:44:04 +0300

    Initial Commit

---
+++ BugSense-iOS/BugSense-iOS.m
@@ -0,0 +1,23 @@
+//
+//  BugSense-iOS.m
+//  BugSense-iOS
+//
+//  Created by Nίκος Τουμπέλης on 23/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "BugSense-iOS.h"
+
+@implementation BugSense_iOS
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
