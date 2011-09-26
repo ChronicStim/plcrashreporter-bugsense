7eb71fcc67fc0322adfa811afb89888ead62af9e
Nick Toumpelis <nick@toumpelis.me.uk>
2011-09-27 01:26:26 +0300

    Implementation start.

--- BugSense-iOS/BugSenseCrashController.m
+++ BugSense-iOS/BugSenseCrashController.m
@@ -12,6 +12,9 @@
 
 - (id) init;
 - (id) initWithAPIKey:(NSString *)bugSenseAPIKey;
+- (void) initiateReportingProcess;
+- (void) processCrashReport;
+- (NSString *) JSONStringFromCrashReport:(PLCrashReport *)report;
 
 @end
 
@@ -106,7 +109,7 @@ static BugSenseCrashController *sharedCrashController = nil;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 - (id) init {
     if ((self = [super init])) {
-        
+        [self initiateReportingProcess];
     }
     return self;
 }
@@ -115,7 +118,8 @@ static BugSenseCrashController *sharedCrashController = nil;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 - (id) initWithAPIKey:(NSString *)bugSenseAPIKey {
     if ((self = [super init])) {
-        
+        _APIKey = [bugSenseAPIKey retain];
+        [self initiateReportingProcess];
     }
     return self;
 }
@@ -126,9 +130,74 @@ static BugSenseCrashController *sharedCrashController = nil;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 - (id) initWithAPIKey:(NSString *)bugSenseAPIKey andDomainName:(NSString *)domainName {
     if ((self = [super init])) {
-        
+        _APIKey = [bugSenseAPIKey retain];
+        [self initiateReportingProcess];
     }
     return self;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Three stages of the process a) make JSON b) send JSON c) smile
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void) initiateReportingProcess {
+    PLCrashReporter *crashReporter = [PLCrashReporter sharedReporter];
+    NSError *error;
+    
+    if ([crashReporter hasPendingCrashReport]) {
+        [self processCrashReport];
+    }
+    
+    if (![crashReporter enableCrashReporterAndReturnError:&error]) {
+        NSLog(@"BugSense --> Warning: Could not enable crash reporterd due to: %@", error);
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void) processCrashReport {
+    PLCrashReporter *crashReporter = [PLCrashReporter sharedReporter];
+    NSData *crashData;
+    NSError *error;
+
+    // Try loading the crash report
+    crashData = [crashReporter loadPendingCrashReportDataAndReturnError:&error];
+    if (!crashData) {
+        NSLog(@"BugSense --> Could not load crash report data due to: %@", error);
+        [crashReporter purgePendingCrashReport];
+        return;
+    }
+    
+    // We could send the report from here, but we'll just print out
+    // some debugging info instead
+    PLCrashReport *report = [[PLCrashReport alloc] initWithData:crashData error:&error];
+    if (!report) {
+        NSLog(@"BugSense --> Could not parse crash report due to: %@", error);
+        [crashReporter purgePendingCrashReport];
+        return;
+    }
+    
+    // Generic status report on console
+    NSLog(@"BugSense --> Crashed on %@", report.systemInfo.timestamp);
+    NSLog(@"BugSense --> Crashed with signal %@ (code %@, address=0x%" PRIx64 ")", report.signalInfo.name, 
+          report.signalInfo.code, report.signalInfo.address);
+    
+    NSString *jsonString = [self JSONStringFromCrashReport:report];
+    
+    if (!jsonString) {
+        // something happened -- error handling
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+- (NSString *) JSONStringFromCrashReport:(PLCrashReport *)report {
+    if (!report) {
+        return nil;
+    }
+    return nil;
 }
 
 @end
